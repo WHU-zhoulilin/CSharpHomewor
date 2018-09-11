46f2d36205538f696405b28f708271e840c5e9e7
zhoulilin <779329434@qq.com>
2018-09-11 22:24:08 +0800

    homework1

---
+++ homework1/homework1/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x, y, max;
+            max = (int)Math.Pow(2, 31);
+            string s = textBox1.Text;
+            x = Int32.Parse(s);
+            s = textBox2.Text;
+            y = Int32.Parse(s);
+            if ((x * y > 0 && x * y < max - 1 && x * 1.0 / max * y / max > 0)
+                || (x * y < 0 && x * y > -max && x * 1.0 / max * y / max < 0))//判断是否超出范围
+            {
+                s = Convert.ToString(x * y);
+                label4.Text = "";
+            }
+            else
+            {
+                label4.Text = ("超出计算范围！");
+                s = "";
+            }
+            label1.Text = (s);
+        }
+    }
+}
---
+++ homework1/program1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            int x, y, max;
+            max = (int)Math.Pow(2, 31);
+            Console.WriteLine("请输入第一个乘数：");
+            s = Console.ReadLine();
+            x = Int32.Parse(s);
+            Console.WriteLine("请输入第二个乘数：");
+            s = Console.ReadLine();
+            y = Int32.Parse(s);
+            if ((x * y > 0 && x * y < max - 1 && x * 1.0 / max * y / max > 0)
+                || (x * y < 0 && x * y > -max && x * 1.0 / max * y / max < 0))//判断是否超出范围
+            {
+                Console.WriteLine("{0} x {1} = {2}", x, y, x * y);
+            }
+            else
+            {
+                Console.WriteLine("超出计算范围！");
+            }
+        }
+    }
+}
